62b13ce463da38a822793c2f41bfafede99ff403
ritik-11182 <64559932+ritik-11182@users.noreply.github.com>
2020-05-25 06:43:50 -0700

    Update Plot2.R
    new one

--- Plot2.R
+++ Plot2.R
@@ -3,5 +3,5 @@ sub <- data[data$Date %in% c("1/2/2007", "2/2/2007") , ]
 global <- as.numeric(sub$Global_active_power)
 date <- strptime(paste(sub$Date, sub$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
 png("Plot2.png", width = 480, height = 480)
-plot(date, global, xlab = "", ylab = "Global Active Power", type = 'l')
+plot(date, global, xlab = "", ylab = "Global Active Power(kilowatts)", type = 'l')
 dev.off()
